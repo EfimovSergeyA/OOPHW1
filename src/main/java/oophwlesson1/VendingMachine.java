e9a4437567b27b3e394d384b92e949007c42276d
EfimovSergeyA <116340503+EfimovSergeyA@users.noreply.github.com>
2023-10-10 19:36:24 +0300

    Homework1

---
+++ src/main/java/oophwlesson1/VendingMachine.java
@@ -0,0 +1,17 @@
+package oophwlesson1;
+
+public abstract class VendingMachine {
+    /**
+     * Внесение продуктов в торговый автомат
+     */
+    void initProduct() {
+
+    }
+
+    /**
+     * Получение продукта из торгового автомата
+     *
+     * @return строку
+     */
+    abstract String getProduct();
+}
